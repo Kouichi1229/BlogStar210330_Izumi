ea2061203f45802c9eae0d4a896edaf30d1b760c
Kouichi <jpr64914@gmail.com>
2021-04-03 19:37:02 +0800

    modeifty model

--- blog/context/admin.py
+++ blog/context/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 from .models import *
-from tinymce.widgets import TinyMCE
+#from tinymce.widgets import TinyMCE
 
 
 
 
-class TutorialAdmin(admin.ModelAdmin):
+# class TutorialAdmin(admin.ModelAdmin):
 
 
-    formfield_overrides = {
-        models.TextField: {'widget': TinyMCE()},
-        }
+#     formfield_overrides = {
+#         models.TextField: {'widget': TinyMCE()},
+#         }
 
 
-
-admin.site.register(Blog_Posts,TutorialAdmin)
+admin.site.register(Blog_Posts)
+# admin.site.register(Blog_Posts,TutorialAdmin)
  
