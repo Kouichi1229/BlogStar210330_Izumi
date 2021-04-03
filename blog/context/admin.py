16f20d5d0a1d90817dbca65139e29104035bb856
Kouichi <jpr64914@gmail.com>
2021-04-03 19:47:36 +0800

    requirements.txt modfity admin

--- blog/context/admin.py
+++ blog/context/admin.py
@@ -5,14 +5,17 @@ from .models import *
 
 
 
-# class TutorialAdmin(admin.ModelAdmin):
+class TutorialAdmin(admin.ModelAdmin):
 
+    fieldsets = [
+        ("Title/date", {'fields': ["tutorial_title", "tutorial_published"]}),
+        ("Content", {"fields": ["tutorial_content"]})
+    ]
 
-#     formfield_overrides = {
-#         models.TextField: {'widget': TinyMCE()},
-#         }
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+        }
 
-
-admin.site.register(Blog_Posts)
-# admin.site.register(Blog_Posts,TutorialAdmin)
+#admin.site.register(Blog_Posts)
+admin.site.register(Blog_Posts,TutorialAdmin)
  
